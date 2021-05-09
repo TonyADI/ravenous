9692fe24ef4420bf625063693c7c98d559b0c68c
TonyADI <35707107+TonyADI@users.noreply.github.com>
2021-05-09 16:43:34 -0400

    Update BusinessList.js

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -6,12 +6,11 @@ export class BusinessList extends React.Component {
     render() {
         return (
             <div className="BusinessList">
-                {this.props.businesses ? 
+                {!this.props.loading ? this.props.businesses ? 
                 this.props.businesses.map(business => 
                 { return <Business key={business.id} business={business}/>}) : 
-                !this.props.loading && <div>No businesses found. 
-                    Try different search parameters</div>}
-                {this.props.loading && <div className="loading">
+                <div>No businesses found. Try different search parameters</div>
+                :<div className="loading">
                     <div/><div/><div/>
                     </div>}
             </div>
